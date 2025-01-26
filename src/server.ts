ffc02886c109fcf1fef33dd65d9bd5c3ddf9b461
Afif Alauddin <afif@rfox.com>
2025-01-26 13:41:13 +0800

    feat(middleware): add logger using pino

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,19 @@
 import express, { type Express } from "express";
+import pino from "pino";
 import errorMiddleware from "~/middleware/error.middleware";
+import requestLogger from "~/middleware/request-logger.middleware";
 
 const app: Express = express();
+
+// Logger
+const logger = pino({ name: "server start" });
 
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Request logging
-// app.use(requestLogger);
+app.use(requestLogger);
 
 // Routes
 
@@ -17,4 +22,4 @@ app.use(express.urlencoded({ extended: true }));
 // Error handlers
 app.use(errorMiddleware());
 
-export { app };
+export { app, logger };
